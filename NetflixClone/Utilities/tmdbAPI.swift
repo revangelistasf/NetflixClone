10f833122c3a782169f6f3e1d79989cd4acb0fc2
Roberto Evangelista <revangelista@innovaj.com>
2021-03-07 23:16:52 +0000

    feat: create TMDBAPI with movieSection and poster URL

--- NetflixClone/Utilities/tmdbAPI.swift
+++ NetflixClone/Utilities/tmdbAPI.swift
@@ -7,6 +7,32 @@
 
 import Foundation
 
-struct TMDBAPI {
+enum TMDBAPI {
+    
+    case movieSection(section: Int)
+    case poster(endpoint: String)
+    
+    private var baseURL: String {
+        switch self {
+        case .movieSection:
+            return "https://api.themoviedb.org/3/discover"
+        case .poster(let endpoint):
+            return "https://image.tmdb.org/t/p/w500\(endpoint)"
+        }
+    }
+    
+    var url: URL? {
+        return URL(string: baseURL)
+    }
+    
+    var queryItems: [URLQueryItem]? {
+        switch self {
+        case .movieSection(let section):
+            return [URLQueryItem(name: "page", value: String(section)),
+                    URLQueryItem(name: "api_key", value: AppConstants.apiKey)]
+        default:
+            return nil
+        }
+    }
     
 }
